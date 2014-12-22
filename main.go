52ae1dafe49ded9e0f8d01cb57a1403fc9727520
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2014-12-22 16:36:06 +0000

    Fixed the convertion rate

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
-package main 
+package main
 
 import "fmt"
 
-
-func main(){
+//celsius to Fahrenheit converter
+//takes a celsius in float64 as a paramete//rturning a float64 after calculations have been made
+func CelToFah(value float64) float64 {
+	var fah float64
+	fah = value*9/5 + 32
+	return fah
+}
+func main() {
 	//declare the users input
+	var userInput float64
+	fmt.Print("Enter your Celsius to be converted to Fahrenheit ")
+	//gets the input from the user
+	fmt.Scanf("%f", &userInput)
+	//format the output from the celsius to Fahrenheit conversion
+	fmt.Printf("Your Fahrenheit is %g F\n", CelToFah(userInput))
 
 }
